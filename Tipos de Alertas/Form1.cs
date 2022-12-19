c27155b133a540de5eed68087e1dccbe6f977b2d
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2022-12-19 11:52:36 -0300

    Primeiro Commit

---
+++ Tipos de Alertas/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tipos_de_Alertas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Sou uma mensagem super simples 😁");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Sou um alerta com título", "Sou o título");
+        }
+
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            {
+                DialogResult retorno = MessageBox.Show("Vamos estudar C#?", "Bora?", MessageBoxButtons.YesNo);
+
+                if (retorno == DialogResult.Yes)
+                {
+                    MessageBox.Show("Legal!!! Bora Estudar!");
+                }
+                else if (retorno == DialogResult.No)
+                {
+                    MessageBox.Show(" :( ");
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DialogResult retorno = MessageBox.Show(
+                "Segue o @prof.tiagotas?",
+                "Serei o Título",
+                MessageBoxButtons.YesNoCancel,
+                MessageBoxIcon.Question
+                );
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ouviu o som do Windows?","Deu algo Errado", 
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
